db3138ec097a25f68b5f36551c0f55ea495c14fd
UraYukimitsu <UraYukimitsu@users.noreply.github.com>
2022-10-04 18:37:08 +0200

    Update jsConsole.js

--- jsConsole.js
+++ jsConsole.js
@@ -1,219 +1,45 @@
-if(typeof fs === 'undefined')
-	fs = undefined;
-if(typeof path === 'undefined')
-	path = undefined;
-if(typeof ofs === 'undefined')
-	ofs = undefined;
-var reqFs = function()
-{
-	if(typeof global === 'undefined')
-		global = window;
-	if(typeof fs == 'undefined' || typeof path == 'undefined' || typeof ofs == 'undefined')
-		try{
-			fs = require('fs');
-			path = require('path');
-			ofs = require('original-fs');
-		} catch (_) { //If !require
-			for(i in global){
-				if(!i.startsWith('temp'))
-					continue;
-				if(typeof global[i][1] == 'object')
-				{
-					if(typeof global[i][1].object.fs != 'undefined')
-						fs = global[i][1].object.fs;
-					if(typeof global[i][1].object.path != 'undefined')
-						path = global[i][1].object.path;
-					if(typeof global[i][1].object.originalFs != 'undefined')
-						ofs = global[i][1].object.originalFs;
-				}
-			}
-		}
-	if(typeof fs == 'undefined' || typeof path == 'undefined')
-	{
-		console.clear();
-		console.error('%cClick on ▶ on the entry below, then RIGHT click on [[Scopes]] and click on "Store as global variable"\nThis script cannot run otherwise.', 'font-size: 250%; color: red;')
-		console.dir(DiscordNative.fileManager.openFiles);
-		return false;
-	}
-	return true;
+function BD(css_path, js_path) {
+    var fm = DiscordNative.fileManager;
+    var f = fm.join(fm.dirname(DiscordNative.processUtils.getMainArgvSync()[0]), 'modules', 'discord_desktop_core-' + DiscordNative.app.getModuleVersions()['discord_desktop_core'], 'discord_desktop_core');
+    var id = `var e = require("fs"),
+    t = require("path"),
+    r = '${f.replace(/\\/g, '\\\\')}';
+    w = e.writeFileSync;
+
+    function o(i, n) {
+        return e.existsSync(n) || e.mkdirSync(n), e.readdirSync(i).forEach((r) => {
+            var c = t.join(i, r);
+            r = t.join(n, r);
+            var a = e.statSync(c);
+            if (a && a.isDirectory()) {
+                if (e.existsSync(r)) {
+                    if (!e.statSync(r).isDirectory()) throw r
+                } else e.mkdirSync(r);
+                o(c, r)
+            } else w(r, e.readFileSync(c))
+        }), !0
+    }
+    o(r + "/core.asar", r + "/c");
+    rs = "mainWindow.webContents.on('dom-ready', function () { \\
+        var _fs = require('fs'); \\
+        var css_path = '${css_path.replace(/\\/g, '\\\\\\\\')}'; \\
+        var js_path = '${js_path.replace(/\\/g, '\\\\\\\\')}'; \\
+        var css = _fs.readFileSync(css_path); \\
+        var sc = _fs.readFileSync(js_path); \\
+        mainWindow.webContents.executeJavaScript('var theme_elem = document.createElement(\\\\'style\\\\');theme_elem.innerHTML = \`' + css + '\`;document.head.appendChild(theme_elem);' + sc); \\
+        _fs.watch(css_path, { encoding: 'utf8' }, function(event, filename) \\
+        { \\
+            if(event !== 'change') return; \\
+            var css = _fs.readFileSync(css_path); \\
+            mainWindow.webContents.executeJavaScript('window.theme_elem.innerHTML = \`' + css + '\`;'); \\
+        }); \\
+    });";
+    et = e.readFileSync(r + "/c/app/mainScreen.js", "utf8")
+    w(r + "/c/app/mainScreenPreload.js", e.readFileSync(r + "/c/app/mainScreenPreload.js", "utf8").replace('process.once(\\'loaded\\',', 'contextBridge.exposeInMainWorld("theme", { uninstall: function() { require("fs").writeFileSync("${fm.join(f, 'index.js').replace(/\\/g, '\\\\\\\\')}", "module.exports = require(\\'./core.asar/app/index.js\\')", "utf8"); DiscordNative.app.relaunch() } })\\nprocess.once(\\'loaded\\','), "utf8")
+    if(et.match(rs) == null) et = et.replace("mainWindow.webContents.on", rs + "\\nmainWindow.webContents.on")
+    w(r + "/c/app/mainScreen.js", et, "utf8")
+    module.exports = require('./c/app/index.js')
+    w(r + "/index.js", "module.exports = require('./c/app/index.js')", "utf8")`;
+    DiscordNative.clipboard.copy(fm.join(f, 'index.js'));
+    fm.saveWithDialog(id, 'Please_paste_the_contents_of_your_clipboard_here', f).then(a => DiscordNative.app.relaunch());
 }
-var BD = function(args)
-{
-	if(!reqFs()) return;
-	restart = reloadElectronApp;
-
-	function copyDir(src, dest)
-	{
-		if(!fs.existsSync(dest))
-			fs.mkdirSync(dest);
-		fs.readdirSync(src).forEach(function(f)
-		{
-			var srcF = path.join(src, f);
-			var destF = path.join(dest, f);
-			var stat = fs.statSync(srcF);
-
-			if(stat && stat.isDirectory())
-			{
-				if(fs.existsSync(destF))
-				{
-					if(!fs.statSync(destF).isDirectory())
-						throw("Error creating " + destF + ": a file with the same name already exists");
-				} else
-					fs.mkdirSync(destF);
-				
-				copyDir(srcF, destF);
-			} else {
-				fs.writeFileSync(destF, fs.readFileSync(srcF));
-			}
-		});
-		return true;
-	}
-
-	function rmDir(dir)
-	{
-		dir = path.resolve(dir);
-		if(fs.exists(dir) && fs.statSync(dir).isDirectory())
-		{	
-			fs.readdirSync(dir).forEach(function(f)
-			{
-				f = path.join(dir, f);
-				if(fs.statSync(f).isDirectory())
-					rmDir(f)
-				else
-					fs.unlinkSync(f);
-			});
-			fs.rmdirSync(dir);
-		}
-	}
-
-	var cwd;
-	try{cwd = path.dirname(require.resolve('discord_desktop_core'))} catch(_) {
-		try{DiscordNative.ipc.on()} catch(e) {
-			cwd = path.dirname(e.stack.slice(e.stack.indexOf('at Object.on') + 14).split('app')[0].slice(0,-1));
-		}
-	}
-	if(args.css)
-		args.css = path.resolve(args.css);
-	else
-		args.css = path.resolve(cwd + '/discord-custom.css');
-
-	if(args.js)
-		args.js = path.resolve(args.js);
-	else
-		args.js = path.resolve(cwd + '/discord-custom.js');
-
-	if(args.revert)
-	{
-		fs.writeFileSync(cwd + '/index.js', "module.exports = require('./core.asar');");
-		restart();
-		return;
-	}
-	copyDir(cwd + '/core.asar', cwd + '/core');
-	if(!fs.existsSync(args.css))
-		fs.writeFileSync(args.css, '/* put your custom css here. */\n', {encoding: 'utf8'});
-	if(!fs.existsSync(args.js))
-		fs.writeFileSync(args.js, '// put your custom JS here.\n', {encoding: 'utf8'});
-
-	injectionScript = `
-window._fs = require("fs");
-window._cssWatcher = null;
-window._styleTag = null;
-window._scriptTag = null;
-window.setupCSS = function (path) {
-	var customCSS = window._fs.readFileSync(path, "utf8");
-	if (window._styleTag === null) {
-		window._styleTag = document.createElement("style");
-		document.head.appendChild(window._styleTag);
-	}
-	window._styleTag.innerHTML = customCSS;
-	if (window._cssWatcher === null) {
-		window._cssWatcher = window._fs.watch(path, {
-				encoding: "utf8"
-			},
-			function (eventType, filename) {
-				if (eventType === "change") {
-					var changed = window._fs.readFileSync(path, "utf8");
-					window._styleTag.innerHTML = changed;
-				}
-			}
-		);
-	}
-};
-window.tearDownCSS = function () {
-	if (window._styleTag !== null) {
-		window._styleTag.innerHTML = "";
-	}
-	if (window._cssWatcher !== null) {
-		window._cssWatcher.close();
-		window._cssWatcher = null;
-	}
-};
-window.applyAndWatchCSS = function (path) {
-	window.tearDownCSS();
-	window.setupCSS(path);
-};
-window.setupJS = function (path) {
-	var customJS = window._fs.readFileSync(path, "utf8");
-	window._script = eval('()=>{'+customJS+'}');
-	window._script();
-};
-window.tearDownJS = function () {
-	if (window._script !== null) {
-		window._script = "";
-	}
-};
-window.applyJS = function (path) {
-	window.tearDownJS();
-	window.setupJS(path);
-};
-window.applyAndWatchCSS('${args.css.replace(/\\/g, '\\\\')}');
-window.applyJS('${args.js.replace(/\\/g, '\\\\')}');
-window.BD = ${BD.toString()}
-window.reqFs = ${reqFs.toString()}
-`;
-	if(args.node)
-		injectionScript += `module.paths.push('${path.resolve(args.node).replace(/\\/g, '\\\\')}')`;
-	
-	fs.writeFileSync(cwd + '/codeInjection.js', injectionScript, {encoding: 'utf8'});
-	injPath = path.resolve(cwd + '/codeInjection.js').replace(/\\/g, '\\\\');
-	reloadScript = `//BeautifulDiscord injection start
-mainWindow.webContents.on('dom-ready', function () {
-	mainWindow.webContents.executeJavaScript(
-		require('fs').readFileSync('${injPath}', 'utf8')
-	);
-});
-//BeautifulDiscord injection end`;
-	var entireThing = fs.readFileSync(cwd + '/core/app/mainScreen.js', 'utf8');
-
-	if(entireThing.match("mainWindow.webContents.on('dom-ready', function () {});") !== null)
-		entireThing = entireThing.replace("mainWindow.webContents.on('dom-ready', function () {});", reloadScript);
-	else if(entireThing.match(reloadScript) === null)
-		entireThing = entireThing.replace("mainWindow.webContents.on", reloadScript + '\nmainWindow.webContents.on');
-	if(args.fixCORS && entireThing.match("webSecurity: false, blinkFeatures: '") === null) {
-		entireThing = entireThing.replace("blinkFeatures: '", "webSecurity: false, blinkFeatures: '");
-		var bootstrap = fs.readFileSync(path.join(DiscordNative.process.remote.resourcesPath, "app.asar", "app_bootstrap", "bootstrap.js"), 'utf-8');
-		bslen = bootstrap.length;
-		obs = bootstrap;
-		bootstrap = bootstrap.replace("const { app, Menu } = require('electron');", `const { app, Menu, protocol } = require('electron');
-		protocol.registerSchemesAsPrivileged([
-			{ scheme: 'data', privileges: { bypassCSP: true } },
-			{ scheme: 'file', privileges: { bypassCSP: true } },
-			{ scheme: 'https', privileges: { bypassCSP: true } },
-			{ scheme: 'http', privileges: { bypassCSP: true } }
-		]);`);
-		while(bootstrap.length > bslen)
-			bootstrap = bootstrap.replace(/\/\/(.+)\n/, "")
-		bootstrap = bootstrap.padEnd(bslen);
-		appAsar = ofs.readFileSync(path.join(DiscordNative.process.remote.resourcesPath, "app.asar"));
-		appAsar.write(bootstrap, appAsar.indexOf(obs))
-		ofs.writeFileSync(path.join(DiscordNative.process.remote.resourcesPath, "app.asar"), appAsar);
-	}
-	entireThing = entireThing.replace('nodeIntegration: false', 'nodeIntegration: true');
-
-	fs.writeFileSync(cwd + '/core/app/mainScreen.js', entireThing, {encoding: 'utf8'});
-
-	fs.writeFileSync(cwd + '/index.js', "module.exports = require('./core/app/index.js');");
-	
-	restart();
-};
-reqFs();
